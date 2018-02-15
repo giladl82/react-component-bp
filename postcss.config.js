73ec9191da0bae26c5816b30e2b12a6beb2e06c1
Gilad Lev Ari <GiladL@cet.ac.il>
2018-02-15 09:16:03 +0200

    Adding nesting+vars to post css

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,8 @@ module.exports = ({ file, options, env }) => ({
   parser: file.extname === '.sss' ? 'sugarss' : false,
   plugins: {
     'postcss-import': { root: file.dirname },
+    'postcss-nested': {},
+    'postcss-variables': {},
     'postcss-cssnext': options.cssnext ? options.cssnext : false,
     'autoprefixer': env == 'production' ? options.autoprefixer : false,
     'cssnano': env === 'production' ? options.cssnano : false
